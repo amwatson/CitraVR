7597481444793dc87a8ecff46a22403c7f49266a
amwatson <contact@fewerwrong.org>
2024-01-24 14:00:08 -0600

    move logging to Citra-android backend

--- src/android/app/src/main/jni/vr/vr_main.cpp
+++ src/android/app/src/main/jni/vr/vr_main.cpp
@@ -35,6 +35,7 @@ License     :   Licensed under GPLv3 or any later version.
 #include <sys/prctl.h>
 #include <unistd.h>
 
+#define USE_INGAME_MENU
 #if defined(DEBUG_INPUT_VERBOSE)
 #define ALOG_INPUT_VERBOSE(...) ALOGI(__VA_ARGS__)
 #else
@@ -95,7 +96,7 @@ void ForwardButtonStateIfNeeded(JNIEnv* jni, jobject activityObject,
     }
 }
 
-const char* XrSessionStateToString(const XrSessionState state) {
+[[maybe_unused]] const char* XrSessionStateToString(const XrSessionState state) {
     switch (state) {
     case XR_SESSION_STATE_UNKNOWN:
         return "XR_SESSION_STATE_UNKNOWN";
@@ -208,6 +209,7 @@ public:
         }
         mCursorLayer = std::make_unique<CursorLayer>(gOpenXr->session_);
         {
+
             const std::string hmdTypeStr =
                 SyspropUtils::GetSysPropAsString("ro.product.model", "unknown");
             const HMDType hmdType = StringToHmdType(hmdTypeStr);
@@ -806,7 +808,7 @@ private:
                       __func__);
                 break;
             case XR_TYPE_EVENT_DATA_PERF_SETTINGS_EXT: {
-                const XrEventDataPerfSettingsEXT* pfs =
+                [[maybe_unused]] const XrEventDataPerfSettingsEXT* pfs =
                     (XrEventDataPerfSettingsEXT*)(baseEventHeader);
                 ALOGV("%s(): Received "
                       "XR_TYPE_EVENT_DATA_PERF_SETTINGS_EXT event: type %d "

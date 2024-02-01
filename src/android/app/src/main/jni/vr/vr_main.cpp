6f7d61b62b70bde1b8be4cfdc5d80dd32c632297
amwatson <contact@fewerwrong.org>
2024-02-01 12:14:40 -0600

    remove debug line

--- src/android/app/src/main/jni/vr/vr_main.cpp
+++ src/android/app/src/main/jni/vr/vr_main.cpp
@@ -888,7 +888,6 @@ Java_org_citra_citra_1emu_vr_VrActivity_nativeOnCreate(JNIEnv* env, jobject thiz
     JavaVM* jvm;
     env->GetJavaVM(&jvm);
     auto ret = VRAppHandle(new VRApp(jvm, env->NewGlobalRef(thiz))).l;
-    ALOGI("amwatson nativeOnCreate {}", ret);
     return ret;
 }
 

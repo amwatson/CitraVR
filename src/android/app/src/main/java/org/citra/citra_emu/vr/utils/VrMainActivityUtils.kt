cbb4c4d6007acec55426857c74176aeffbca80f6
amwatson <contact@fewerwrong.org>
2024-02-07 18:16:16 -0600

    [version update] always delete ini.vr0 file when present

--- src/android/app/src/main/java/org/citra/citra_emu/vr/utils/VrMainActivityUtils.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/vr/utils/VrMainActivityUtils.kt
@@ -7,19 +7,9 @@ import org.citra.citra_emu.utils.Log
 
 object VrMainActivityUtils {
     fun doVersionUpdates(applicationContext: Context) {
-        val preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val releaseVersionPrev : VrReleaseVersion = VrReleaseVersion(preferences.getString(VRUtils.PREF_RELEASE_VERSION_NAME_LAUNCH_PREV, "") ?: "")
-        val releaseVersionCur : VrReleaseVersion = VrReleaseVersion(preferences.getString(VRUtils.PREF_RELEASE_VERSION_NAME_LAUNCH_CURRENT, "") ?: "")
-        // If the previously run build was lower that v0.4.0 (note: all these builds will not have valid release versions), wipe the config. This will
-        // remove previous issues like the dpad
-        // Note: Do the version check whenever the current build has a valid release tag
-        if (releaseVersionCur.isRealVersion() && !releaseVersionCur.hasLowerVersionThan(VrReleaseVersion.RELEASE_VERSION_0_4_0) &&  (!releaseVersionPrev.isRealVersion() || releaseVersionPrev.hasLowerVersionThan(
-                VrReleaseVersion.RELEASE_VERSION_0_4_0))) {
-            Log.info("New install from prev version \"v${releaseVersionCur.getMajor()}.${releaseVersionCur.getMinor()}.${releaseVersionCur.getPatch()}\" needs update. Wiping config.ini.vr0")
             // Delete V0.3.2 settings file if present
             try {
                 SettingsFile.getSettingsFile(SettingsFile.FILE_NAME_CONFIG, "ini.vr0").delete()
             } catch (e : Exception) {}
-        }
     }
 }

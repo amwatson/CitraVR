acf1c74d342993a5ed17d017b402385f022fd560
amwatson <contact@fewerwrong.org>
2024-02-09 18:44:56 -0600

    Fix keyboard formatting

--- src/android/app/src/main/java/org/citra/citra_emu/vr/ui/VrKeyboardLayer.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/vr/ui/VrKeyboardLayer.kt
@@ -1,5 +1,9 @@
 package org.citra.citra_emu.vr.ui
 
+import android.view.View
+import android.widget.EditText
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import org.citra.citra_emu.R
 import org.citra.citra_emu.vr.VrActivity
 
@@ -7,5 +11,12 @@ class VrKeyboardLayer(activity: VrActivity) : VrUILayer(activity, R.layout.vr_ke
 
     override fun onSurfaceCreated() {
         super.onSurfaceCreated()
+        var editText : EditText = window?.findViewById<View>(R.id.vrKeyboardText) as EditText
+        editText!!.apply {
+            // Needed to show cursor onscreen.
+            requestFocus()
+            WindowCompat.getInsetsController(window!!, window?.decorView!!)
+                .show(WindowInsetsCompat.Type.ime())
+        }
     }
 }

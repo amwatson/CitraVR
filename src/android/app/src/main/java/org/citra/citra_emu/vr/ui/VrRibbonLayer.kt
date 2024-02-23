dde7d9f3ca75c93f9441ebcf29750683a69a7a14
amwatson <contact@fewerwrong.org>
2024-02-22 20:38:47 -0600

    add quit and next buttons

--- src/android/app/src/main/java/org/citra/citra_emu/vr/ui/VrRibbonLayer.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/vr/ui/VrRibbonLayer.kt
@@ -18,19 +18,6 @@ class VrRibbonLayer(activity: VrActivity) : VrUILayer(activity, R.layout.vr_ribb
                 else NativeLibrary.ButtonState.RELEASED)
             false
         }
-        window?.findViewById<ImageButton>(R.id.buttonHome)?.setOnTouchListener { view, motionEvent ->
-            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
-                (view as ImageButton).setImageResource(R.drawable.button_home_pressed)
-            } else {
-                (view as ImageButton).setImageResource(R.drawable.button_home)
-            }
-            NativeLibrary.onGamePadEvent(NativeLibrary.TouchScreenDevice,
-                NativeLibrary.ButtonType.BUTTON_HOME,
-                if (motionEvent.action == MotionEvent.ACTION_DOWN)
-                    NativeLibrary.ButtonState.PRESSED
-                else NativeLibrary.ButtonState.RELEASED)
-            false
-        }
         window?.findViewById<Button>(R.id.buttonStart)?.setOnTouchListener { _, motionEvent ->
             NativeLibrary.onGamePadEvent(NativeLibrary.TouchScreenDevice,
                 NativeLibrary.ButtonType.BUTTON_START,

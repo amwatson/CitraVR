a70a7a42907147c6fa54f44b32f0624e3c73c1f9
amwatson <contact@fewerwrong.org>
2024-03-19 16:01:18 -0500

    [UI Ribbon] move prev and next buttons to underneath panel

--- src/android/app/src/main/java/org/citra/citra_emu/vr/ui/VrRibbonLayer.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/vr/ui/VrRibbonLayer.kt
@@ -1,5 +1,6 @@
 package org.citra.citra_emu.vr.ui
 
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.ImageButton
@@ -10,24 +11,45 @@ import org.citra.citra_emu.vr.VrActivity
 class VrRibbonLayer(activity: VrActivity) : VrUILayer(activity, R.layout.vr_ribbon) {
        override fun onSurfaceCreated() {
         super.onSurfaceCreated()
+        initializeMainView()
+    }
+
+    fun initializeMainView() {
         window?.findViewById<Button>(R.id.buttonSelect)?.setOnTouchListener { _, motionEvent ->
+            val action: Int = when (motionEvent.action) {
+                KeyEvent.ACTION_DOWN -> {
+                    // Normal key events.
+                    NativeLibrary.ButtonState.PRESSED
+                }
+                KeyEvent.ACTION_UP -> NativeLibrary.ButtonState.RELEASED
+                else -> return@setOnTouchListener false
+            }
             NativeLibrary.onGamePadEvent(NativeLibrary.TouchScreenDevice,
                 NativeLibrary.ButtonType.BUTTON_SELECT,
-                if (motionEvent.action == MotionEvent.ACTION_DOWN)
-                    NativeLibrary.ButtonState.PRESSED
-                else NativeLibrary.ButtonState.RELEASED)
+                action)
             false
         }
         window?.findViewById<Button>(R.id.buttonStart)?.setOnTouchListener { _, motionEvent ->
+            val action: Int = when (motionEvent.action) {
+                KeyEvent.ACTION_DOWN -> {
+                    // Normal key events.
+                    NativeLibrary.ButtonState.PRESSED
+                }
+                KeyEvent.ACTION_UP -> NativeLibrary.ButtonState.RELEASED
+                else -> return@setOnTouchListener false
+            }
             NativeLibrary.onGamePadEvent(NativeLibrary.TouchScreenDevice,
                 NativeLibrary.ButtonType.BUTTON_START,
-                if (motionEvent.action == MotionEvent.ACTION_DOWN)
-                    NativeLibrary.ButtonState.PRESSED
-                else NativeLibrary.ButtonState.RELEASED)
+                action)
             false
         }
         window?.findViewById<Button>(R.id.buttonExit)?.setOnTouchListener { _, motionEvent ->
             activity.quitToMenu()
+            false
+        }
+
+        window?.findViewById<Button>(R.id.buttonNextMenu)?.setOnTouchListener { _, motionEvent ->
+
             false
         }
     }

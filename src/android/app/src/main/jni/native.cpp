7d0346a07b805b5ee841b472697c8de2177da894
OpenSauce04 <opensauce04@gmail.com>
2024-08-23 12:39:30 +0100

    Fixed swapping screens on landscape displays forcing portrait layout

--- src/android/app/src/main/jni/native.cpp
+++ src/android/app/src/main/jni/native.cpp
@@ -54,6 +54,7 @@
 #include "jni/id_cache.h"
 #include "jni/input_manager.h"
 #include "jni/ndk_motion.h"
+#include "jni/util.h"
 #include "video_core/debug_utils/debug_utils.h"
 #include "video_core/gpu.h"
 #include "video_core/renderer_base.h"
@@ -369,7 +370,7 @@ void Java_org_citra_citra_1emu_NativeLibrary_swapScreens([[maybe_unused]] JNIEnv
     Settings::values.swap_screen = swap_screens;
     auto& system = Core::System::GetInstance();
     if (system.IsPoweredOn()) {
-        system.GPU().Renderer().UpdateCurrentFramebufferLayout(!(rotation % 2));
+        system.GPU().Renderer().UpdateCurrentFramebufferLayout(IsPortraitMode());
     }
     InputManager::screen_rotation = rotation;
     Camera::NDK::g_rotation = rotation;

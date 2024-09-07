070c1b490fd3660bfd1552f6ba538af8da6ad739
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-09-07 13:40:18 -0600

    Android: Update NDK from r26d to r27b

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     namespace = "org.citra.citra_emu"
 
     compileSdkVersion = "android-34"
-    ndkVersion = "26.3.11579264"
+    ndkVersion = "27.1.12297006"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17

323d0b209ce317c72b8343516d17d2610abedfc0
FearlessTobi <thm.frey@gmail.com>
2024-11-07 15:36:23 +0000

    citra_qt: Add support for game desktop shortcuts

--- src/citra_qt/loading_screen.cpp
+++ src/citra_qt/loading_screen.cpp
@@ -15,6 +15,7 @@
 #include <QTime>
 #include <fmt/format.h>
 #include "citra_qt/loading_screen.h"
+#include "citra_qt/util/util.h"
 #include "common/logging/log.h"
 #include "core/loader/loader.h"
 #include "core/loader/smdh.h"
@@ -78,18 +79,6 @@ const static std::unordered_map<VideoCore::LoadCallbackStage, const char*> progr
     {VideoCore::LoadCallbackStage::Build, PROGRESSBAR_STYLE_BUILD},
     {VideoCore::LoadCallbackStage::Complete, PROGRESSBAR_STYLE_COMPLETE},
 };
-
-static QPixmap GetQPixmapFromSMDH(std::vector<u8>& smdh_data) {
-    Loader::SMDH smdh;
-    std::memcpy(&smdh, smdh_data.data(), sizeof(Loader::SMDH));
-
-    bool large = true;
-    std::vector<u16> icon_data = smdh.GetIcon(large);
-    const uchar* data = reinterpret_cast<const uchar*>(icon_data.data());
-    int size = large ? 48 : 24;
-    QImage icon(data, size, size, QImage::Format::Format_RGB16);
-    return QPixmap::fromImage(icon);
-}
 
 LoadingScreen::LoadingScreen(QWidget* parent)
     : QWidget(parent), ui(std::make_unique<Ui::LoadingScreen>()),

9bdc929cf6f109455ca3ad04d9ac5a85c8ce4420
OpenSauce04 <opensauce04@gmail.com>
2025-04-08 00:31:50 +0100

    Updated license headers

--- src/citra_qt/util/util.cpp
+++ src/citra_qt/util/util.cpp
@@ -1,4 +1,4 @@
-// Copyright Citra Emulator Project / Lime3DS Emulator Project
+// Copyright Citra Emulator Project / Azahar Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 

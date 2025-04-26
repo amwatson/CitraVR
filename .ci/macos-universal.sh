fcd5c4a29893cd46597b7418af9e58e161144d4b
OpenSauce04 <opensauce04@gmail.com>
2025-04-26 22:24:51 +0100

    ci: Don't compile standalone room for MacOS
    Closes #672

--- .ci/macos-universal.sh
+++ .ci/macos-universal.sh
@@ -11,7 +11,7 @@ BASE_ARTIFACT_ARCH="${BASE_ARTIFACT##*-}"
 mv $BASE_ARTIFACT $BUNDLE_DIR
 
 # Executable binary paths that need to be combined.
-BIN_PATHS=(azahar-room Azahar.app/Contents/MacOS/azahar)
+BIN_PATHS=(Azahar.app/Contents/MacOS/azahar)
 
 # Dylib paths that need to be combined.
 IFS=$'\n'
@@ -37,7 +37,7 @@ for OTHER_ARTIFACT in "${ARTIFACTS_LIST[@]:1}"; do
 done
 
 # Re-sign executables and bundles after combining.
-APP_PATHS=(azahar-room Azahar.app)
+APP_PATHS=(Azahar.app)
 for APP_PATH in "${APP_PATHS[@]}"; do
     codesign --deep -fs - $BUNDLE_DIR/$APP_PATH
 done

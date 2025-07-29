eee315d583dd799e9f3ff27223026fce42d825dc
OpenSauce04 <opensauce04@gmail.com>
2025-07-30 00:54:49 +0100

    android: Upgrade to AGP 8.11.1, Gradle 8.13

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -4,14 +4,14 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.8.1" apply false
-    id("com.android.library") version "8.8.1" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
 }
 
 tasks.register("clean").configure {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
 
 buildscript {

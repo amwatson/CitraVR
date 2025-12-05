8a48e1d6a7057641faec481ff49690c1bd48076f
OpenSauce04 <opensauce04@gmail.com>
2025-12-05 13:28:08 +0000

    ci: Split AppImage build into two, with and without Wayland support

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -ex
 
-if [ "$TARGET" = "appimage" ]; then
+if [[ "$TARGET" == "appimage"* ]]; then
     # Compile the AppImage we distribute with Clang.
     export EXTRA_CMAKE_FLAGS=(-DCMAKE_CXX_COMPILER=clang++
                               -DCMAKE_C_COMPILER=clang
                               -DCMAKE_LINKER=/etc/bin/ld.lld
                               -DENABLE_ROOM_STANDALONE=OFF)
-    # Bundle required QT wayland libraries
-    export EXTRA_QT_PLUGINS="waylandcompositor"
-    export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so"
+    if [ "$TARGET" = "appimage-wayland" ]; then
+        # Bundle required QT wayland libraries
+        export EXTRA_QT_PLUGINS="waylandcompositor"
+        export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so"
+    fi
 else
     # For the linux-fresh verification target, verify compilation without PCH as well.
     export EXTRA_CMAKE_FLAGS=(-DCITRA_USE_PRECOMPILED_HEADERS=OFF)
@@ -30,7 +32,7 @@ cmake .. -G Ninja \
 ninja
 strip -s bin/Release/*
 
-if [ "$TARGET" = "appimage" ]; then
+if [[ "$TARGET" == "appimage"* ]]; then
     ninja bundle
     # TODO: Our AppImage environment currently uses an older ccache version without the verbose flag.
     ccache -s

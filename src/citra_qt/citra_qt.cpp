a64324aba88d3c837c53d1d7e86896d59017a306
RedBlackAka <140876408+RedBlackAka@users.noreply.github.com>
2026-02-17 10:32:01 +0100

    Windows: Fix game shortcut character corruption (#1745)

--- src/citra_qt/citra_qt.cpp
+++ src/citra_qt/citra_qt.cpp
@@ -1918,7 +1918,9 @@ bool GMainWindow::CreateShortcutLink(const std::filesystem::path& shortcut_path,
         LOG_ERROR(Frontend, "Failed to get IPersistFile interface");
         return false;
     }
-    hres = persist_file->Save(std::filesystem::path{shortcut_path / (name + ".lnk")}.c_str(), TRUE);
+    hres = persist_file->Save(
+        std::filesystem::path{shortcut_path / (Common::UTF8ToUTF16W(name) + L".lnk")}.c_str(),
+        TRUE);
     if (FAILED(hres)) {
         LOG_ERROR(Frontend, "Failed to save shortcut");
         return false;

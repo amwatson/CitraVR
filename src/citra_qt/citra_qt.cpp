9956093bed379113f7f14c8dc04218ba7860c07b
OpenSauce04 <opensauce04@gmail.com>
2026-06-29 13:37:47 +0100

    qt: Fix macOS freeze-on-boot caused by unprocessed events
    As far as I can tell the line added here really shouldn't be necessary and wasn't until a relatively recent change in either Qt or macOS. The exact reason behind the original issue is unclear, but by educated guess it could have been caused by some kind of race condition within Qt?
    
    (cherry picked from commit 7c225c6abb9802f81dccb1ea8e32d1c05a63a538)

--- src/citra_qt/citra_qt.cpp
+++ src/citra_qt/citra_qt.cpp
@@ -4456,6 +4456,9 @@ int LaunchQtFrontend(int argc, char* argv[]) {
     QObject::connect(&app, &QGuiApplication::applicationStateChanged, &main_window,
                      &GMainWindow::OnAppFocusStateChanged);
 
+    // Process any pending events before executing the app (prevents freeze-on–boot on macOS)
+    app.processEvents();
+
     int result = app.exec();
     return result;
 }

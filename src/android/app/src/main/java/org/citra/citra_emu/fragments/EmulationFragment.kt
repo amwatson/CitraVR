234104d28e459e47a85d823d9133031d2d132d70
OpenSauce04 <opensauce04@gmail.com>
2026-07-04 15:38:38 +0100

    android: Fix Turbo button not appearing in Adjust Scale menu
    (cherry picked from commit 2a39345e4b5ec81b33b1af3b4e572e7c031155b9)

--- src/android/app/src/main/java/org/citra/citra_emu/fragments/EmulationFragment.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/fragments/EmulationFragment.kt
@@ -877,6 +877,11 @@ class EmulationFragment :
                     true
                 }
 
+                R.id.menu_emulation_adjust_scale_button_turbo -> {
+                    showAdjustScaleDialog("controlScale-" + NativeLibrary.ButtonType.BUTTON_TURBO)
+                    true
+                }
+
                 R.id.menu_emulation_adjust_scale_button_combo -> {
                     showAdjustScaleDialog("controlScale-" + Hotkey.COMBO_BUTTON.button)
                     true
@@ -1435,6 +1440,7 @@ class EmulationFragment :
         resetScale("controlScale-" + NativeLibrary.ButtonType.STICK_C)
         resetScale("controlScale-" + NativeLibrary.ButtonType.BUTTON_HOME)
         resetScale("controlScale-" + NativeLibrary.ButtonType.BUTTON_SWAP)
+        resetScale("controlScale-" + NativeLibrary.ButtonType.BUTTON_TURBO)
         resetScale("controlScale-" + Hotkey.COMBO_BUTTON.button)
         binding.surfaceInputOverlay.refreshControls()
     }
